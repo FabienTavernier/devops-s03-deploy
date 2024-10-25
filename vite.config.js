9f31c89405321a77d0b3a3133537c4f8ba4e8416
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2024-10-25 08:55:01 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    svelte(),
+    svelteTesting()
+  ],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest-setup.js'],
+  },
+})
